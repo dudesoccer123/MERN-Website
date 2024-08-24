58035e4f6bd8a4fc939e0c008eea9c1b633f4ebf
Sarvadnya Deshpande <sarvadnyadeshpande24@gmail.com>
2024-08-24 20:12:38 +0530

    NAVBAR issue sorted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Box, } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import {useColorModeValue  } from '@chakra-ui/react';
 
 function App() {
