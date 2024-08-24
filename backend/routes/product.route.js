a1200a84892a28e5b5fcf026ae3532f2f848ddd7
Sarvadnya Deshpande <sarvadnyadeshpande24@gmail.com>
2024-08-24 19:57:11 +0530

    Project completed

---
+++ backend/routes/product.route.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+
+
+// import functions from controller. 
+
+import { getProducts,createProduct,updateProduct , deleteProduct} from "../controllers/product.controller.js";
+
+
+router.get("/", getProducts)
+
+router.post("/", createProduct);
+
+router.put("/:id" , updateProduct);
+
+router.delete("/:id",deleteProduct);
+
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import dotenv from "dotenv";
+import {connectDB} from './config/db.js';
+import Product from "./models/product.model.js";
+import path from "path";
+
+import productRoutes from "./routes/product.route.js";
+
+
+dotenv.config();
+const app = express() ;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows to accept json data in a function body. 
+// the above function is a middleware function. 
+
+// api endpoint to get all the products in the database. 
+
+app.use("/api/products",productRoutes);
+
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"/frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"));
+        
+    })
+}
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("server started at http://localhost:"+PORT);
+});
+
+//
